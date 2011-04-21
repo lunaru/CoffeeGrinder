a87f3cee2af046f25e792332ebb4bcaa942603f1
lunaru <lwang@corp.oodle.com>
2011-04-21 14:15:25 -0700

    Initial check-in

---
+++ lib/CoffeeGrinder.php
@@ -0,0 +1,38 @@
+<?php
+
+class CoffeeGrinder
+{
+	protected $compiler;
+
+	public function __construct($compilerCmd = 'java -jar jcoffeescript-1.0.jar')
+	{
+		$this->compiler = $compilerCmd;
+	}
+
+	public function parse($coffee)
+	{
+		$descriptorspec = array(
+			0 => array('pipe', 'r'),
+			1 => array('pipe', 'w'),
+			2 => array('pipe', 'w')
+		);
+
+		$process = proc_open($this->compiler, $descriptorspec, $pipes);
+
+		if (is_resource($process))
+		{
+			fwrite($pipes[0], $coffee);
+			fclose($pipes[0]);
+
+			$retval = stream_get_contents($pipes[1]);
+
+			fclose($pipes[1]);
+			fclose($pipes[2]);
+
+			$proc_value = proc_close($process);
+			
+			return $retval;
+		}
+	}
+}
+
